5432346814d758ec32d176619ce9c518bf4077fb
GWillemsen <33036522+GWillemsen@users.noreply.github.com>
2019-10-17 16:27:57 +0200

    Added basic Arduino layout

---
+++ FlightSim/FlightSim/Sketch.cpp
@@ -0,0 +1,17 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
